31c4b7ed24481f907fc827d0cbc46e21b0100b87
snowcrab382 <snowcrab382@naver.com>
2025-03-14 03:27:03 +0900

    refactor: 도서 도메인 유효성 검증 로직 추가 - 도서 필드 필수 입력값 검증 애노테이션 추가 - 도서 생성 시 isbn 필드 고유성 검증 로직 구현

--- src/main/java/books/management/domain/book/dto/request/BookRequestDto.java
+++ src/main/java/books/management/domain/book/dto/request/BookRequestDto.java
@@ -13,19 +13,19 @@ import org.springframework.format.annotation.DateTimeFormat;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class BookRequestDto {
 
-    @NotNull
+    @NotNull(message = "제목은 필수 입력 값입니다.")
     private String title;
     private String description;
 
-    @NotNull
+    @NotNull(message = "ISBN은 필수 입력 값입니다.")
     @Length(min = 10, max = 10)
-    @Pattern(regexp = "^(?:10|[1-8][0-9]|90)[0-9]{7}0$")
+    @Pattern(regexp = "^(?:10|[1-8][0-9]|90)[0-9]{7}0$", message = "ISBN 형식에 맞지 않습니다.")
     private String isbn;
 
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate publicationDate;
 
-    @NotNull
+    @NotNull(message = "저자 ID는 필수 입력 값입니다.")
     private Long authorId;
 
     private BookRequestDto(String title, String description, String isbn, LocalDate publicationDate, Long authorId) {
