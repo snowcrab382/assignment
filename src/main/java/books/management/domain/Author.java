092269bbe2f7e45397c15eeddec265472ec0b57d
임동훈 <snowcrab382@naver.com>
2025-03-13 17:22:49 +0900

    chore: 엔티티 모델링 (#4)
    * chore: Author 엔티티 구현
    
    * chore: Book 엔티티 구현
    
    * chore: Author와 Book 연관 관계 설정
    - 저자와 도서 간 일대다 관계 정의
    - 연관관계에 따른 도서 삭제 정책 옵션은 추후 재정의

---
+++ src/main/java/books/management/domain/Author.java
@@ -0,0 +1,35 @@
+package books.management.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "author")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @OneToMany(mappedBy = "author")
+    private List<Book> books = new ArrayList<>();
+    
+}
