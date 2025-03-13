0ddff1b2c86609380c8c4adb02b85055f552e144
snowcrab382 <snowcrab382@naver.com>
2025-03-13 15:55:41 +0900

    chore: Book 엔티티 구현

---
+++ src/main/java/books/management/domain/Book.java
@@ -0,0 +1,34 @@
+package books.management.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "book")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    private String description;
+
+    @Column(nullable = false, unique = true)
+    private Long isbn;
+
+    private LocalDateTime publicationDate;
+
+}
