5571a0408b99cde887379001e52aeabe6fecf548
snowcrab382 <snowcrab382@naver.com>
2025-03-14 08:12:52 +0900

    refactor: 도서 도메인 유효성 검증 조건 수정 - 도서 제목과 ISBN에 빈 문자열을 입력해도 통과되던 문제 수정 - 출판일 형식과 일치하지 않는 경우를 핸들링하는 커스텀 예외핸들러 구현

--- src/main/java/books/management/global/error/response/GlobalErrorCode.java
+++ src/main/java/books/management/global/error/response/GlobalErrorCode.java
@@ -9,6 +9,7 @@ public enum GlobalErrorCode implements ErrorCode {
 
     INTERNAL_SERVER_ERROR(500, "서버에서 핸들링되지 않은 에러가 발생하였습니다."),
     METHOD_ARGUMENT_NOT_VALID(400, "요청 값이 유효하지 않습니다."),
+    HTTP_MESSAGE_NOT_READABLE(400, "요청 값이 유효하지 않습니다. 입력 형식이 올바른지 확인하세요."),
     METHOD_NOT_ALLOWED(405, "허용되지 않은 메소드입니다."),
 
     AUTHOR_NOT_FOUND(400, "저자를 찾을 수 없습니다."),
