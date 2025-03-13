ddb6d764f45dc34cb8ba4f28d1e93a3444402a82
snowcrab382 <snowcrab382@naver.com>
2025-03-13 15:55:02 +0900

    chore: Author 엔티티 구현

---
+++ src/main/java/books/management/domain/Author.java
@@ -0,0 +1,28 @@
+package books.management.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "author")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+}
