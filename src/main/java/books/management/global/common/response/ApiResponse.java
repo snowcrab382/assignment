9e98288b8d73fbcbfe27724d6993e961fec9c19f
snowcrab382 <snowcrab382@naver.com>
2025-03-14 04:45:17 +0900

    feat: API 공통 응답 포맷 구현

---
+++ src/main/java/books/management/global/common/response/ApiResponse.java
@@ -0,0 +1,30 @@
+package books.management.global.common.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class ApiResponse<T> {
+
+    private int status;
+    private String message;
+    private T data;
+
+    public static <T> ApiResponse<T> of(ResponseCode responseCode) {
+        return ApiResponse.<T>builder()
+                .status(responseCode.getStatus())
+                .message(responseCode.getMessage())
+                .data(null)
+                .build();
+    }
+
+    public static <T> ApiResponse<T> of(ResponseCode responseCode, T data) {
+        return ApiResponse.<T>builder()
+                .status(responseCode.getStatus())
+                .message(responseCode.getMessage())
+                .data(data)
+                .build();
+    }
+
+}
