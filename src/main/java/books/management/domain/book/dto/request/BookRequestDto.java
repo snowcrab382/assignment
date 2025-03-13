3453b0e3471ddae51f2ecefa690e093bb5c6cc01
snowcrab382 <snowcrab382@naver.com>
2025-03-14 02:07:06 +0900

    refactor: 도서 엔티티 유효성 검증 설정 - 도서 생성 및 수정 시 유효성 검증 실시 - 필수 입력값 설정 - isbn-10 규칙 적용 - 출판일 날짜 형식 제약사항 적용

--- src/main/java/books/management/domain/book/dto/request/BookRequestDto.java
+++ src/main/java/books/management/domain/book/dto/request/BookRequestDto.java
@@ -1,18 +1,31 @@
 package books.management.domain.book.dto.request;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import java.time.LocalDate;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class BookRequestDto {
 
+    @NotNull
     private String title;
     private String description;
+
+    @NotNull
+    @Length(min = 10, max = 10)
+    @Pattern(regexp = "^(?:10|[1-8][0-9]|90)[0-9]{7}0$")
     private String isbn;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate publicationDate;
+
+    @NotNull
     private Long authorId;
 
     private BookRequestDto(String title, String description, String isbn, LocalDate publicationDate, Long authorId) {
