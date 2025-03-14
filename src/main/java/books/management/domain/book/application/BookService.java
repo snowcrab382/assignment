0b5bb28691f5cd29c6871257b794aff78ba4bd17
snowcrab382 <snowcrab382@naver.com>
2025-03-14 11:49:45 +0900

    document: 도서 도메인 서비스 레이어 주석 작성

--- src/main/java/books/management/domain/book/application/BookService.java
+++ src/main/java/books/management/domain/book/application/BookService.java
@@ -22,6 +22,11 @@ public class BookService {
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
 
+    /**
+     * 새로운 도서를 생성합니다. - ISBN의 고유성을 유지하기 위해 가장 먼저 데이터베이스에 동일한 값이 존재하는지 확인하는 로직이 실행됩니다.
+     *
+     * @param request
+     */
     public void create(BookRequestDto request) {
         validateIsbn(request.getIsbn());
         Author author = findAuthorById(request.getAuthorId());
@@ -29,6 +34,11 @@ public class BookService {
         bookRepository.save(book);
     }
 
+    /**
+     * 저장된 모든 도서 목록을 조회합니다. - findAll() 메서드를 사용하기 때문에 해당 테이블의 모든 데이터를 조회하는 것이므로, 데이터가 많을 경우 성능 문제가 발생할 수 있습니다.
+     *
+     * @return
+     */
     @Transactional(readOnly = true)
     public List<BookResponseDto> findAllBooks() {
         List<Book> allBooks = bookRepository.findAll();
@@ -43,6 +53,12 @@ public class BookService {
         return BookResponseDto.from(book);
     }
 
+    /**
+     * 도서 상세 정보를 수정합니다. - 도서의 ISBN이 변경되었을 경우, ISBN의 고유성 검증 로직을 실행합니다. - 도서와 연관 관계를 맺는 저자 정보가 존재하지 않을 경우 예외가 발생합니다.
+     *
+     * @param id
+     * @param request
+     */
     public void updateBookDetails(Long id, BookRequestDto request) {
         Book book = findById(id);
 
@@ -59,6 +75,11 @@ public class BookService {
         bookRepository.deleteById(id);
     }
 
+    /**
+     * ISBN의 고유성을 검증하기 위해 데이터베이스에 동일한 ISBN이 존재하는지 확인합니다.
+     *
+     * @param isbn
+     */
     private void validateIsbn(String isbn) {
         if (bookRepository.existsByIsbn(isbn)) {
             throw new NonUniqueValueException(GlobalErrorCode.BOOK_ISBN_DUPLICATION);
